56099208694759ab2f4ebeb39dba5cea50aa09b9
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-09-16 14:49:51 +0530

    Q1

---
+++ 2.c
@@ -0,0 +1,31 @@
+// Binary Search in C
+
+#include <stdio.h>
+
+int push();
+int pop();
+int traverse();
+
+int main (){
+
+
+}
+
+
+int push(){
+
+
+}
+
+
+int pop(){
+
+
+}
+
+
+int traverse(){
+
+
+}
+
---
+++ Q1.c
@@ -0,0 +1,66 @@
+// Linear Search in C
+
+#include <stdio.h>
+
+
+int linearSearch();
+int binarySearch();
+
+
+int main() {
+    int arr[] = {5,3,1,6,9,0};
+    int index,index1;
+
+    printf("Enter the number to be searched : ");
+    scanf("%d", &index);
+
+    printf("Enter the number to be searched : ");
+    scanf("%d", &index1);
+    
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+    int low, high;
+    int outputLs = linearSearch(arr, arrSize, index);
+    int outputBs = binarySearch(arr, index1 , 0 , arrSize);
+
+    printf("Linear Search Output- \n");
+    (outputLs == -1) ? printf("Number not present in array") : printf("Number is present at index: %d", outputLs);
+
+    printf("\n");
+    printf("\n");
+
+    printf("Binary Search Output- \n");
+    (outputBs == -1) ? printf("Number not present in array") : printf("Number is present at index: %d", outputBs);
+    return 0;
+
+
+}
+
+// O(n) - time complexity
+// O(1) - space complexity
+int linearSearch(int arr[], int arrSize, int index) {
+    for (int i = 0; i < arrSize; i++){
+        if (arr[i] == index)
+            return i;
+    }
+    return -1;
+}
+
+
+// O(log(n)) - time complexity
+// O(1) - space complexity
+int binarySearch(int arr[], int index1, int low, int high) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+    if (arr[mid] == index1)
+        return mid;
+
+    if (arr[mid] < index1)
+        low = mid + 1;
+
+    else
+        high = mid - 1;
+    }
+
+    return -1;
+}
