28c0a43b5a48963b6ebd594920f0807eb6839191
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-09-19 23:57:19 +0530

    code

---
+++ prolog/db.pl
@@ -0,0 +1,19 @@
+
+loves(romeo , juliet).
+
+loves(juliet , romeo) :- loves(romeo , juliet).
+
+male(albert).
+male(adam).
+male(prolog).
+
+
+female(sakshi).
+female(shreya).
+female(arushi).
+female(aryushi).
+
+
+
+
+
