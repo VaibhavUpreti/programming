8d9f82a0f346ef1e95e039219bff729794e1f42d
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-09-21 21:36:13 +0530

    practice

--- prolog/practice/twofactorial.pl
+++ prolog/practice/twofactorial.pl
@@ -1,21 +1,11 @@
 
-factorial(0,1).
-
-factorial(A,B) :-
-	A>0,
-	C is A-1,
-	factorial(C,D),
-	B is A*D.
-
-
-
 twofactorial(0,F,F).
+
 
 twofactorial(N,A,F) :-
 	N>0,
 	A1 is N*A,
 	N1 is N-1,
 	twofactorial(N1,A1,F).
-
 
 
