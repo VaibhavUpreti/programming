8ce9364fe3c4d882821510739b8d4e471f602ecb
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2022-09-29 22:23:58 +0530

    cm

---
+++ Python/Q6.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    # print(csv_reader) prints the variable adsress
+    for line in csv_reader:
+        print(line)
+
