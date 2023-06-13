fd16d44be0d3998d43ca0c4bcc2fb9a2b17c6806
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-06-13 19:40:50 +0530

    feat exp 25
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

---
+++ java/Lab25.java
@@ -0,0 +1,10 @@
+package Lab25;
+class Lab25 {
+	public static void main(String args[]) {
+	  System.out.println("Vaibhav Upreti 029");
+		MultiThread m1 = new MultiThread();
+		MultiThread m2 = new MultiThread();
+		m1.start();
+		m2.start();
+	}
+}
