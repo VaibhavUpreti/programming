6f3fcb38bf87129a6fb6226a2c5fc57b5adbde74
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-06-13 19:55:58 +0530

    feat exp 25
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

--- java/Lab25.java
+++ java/Lab25.java
@@ -8,3 +8,19 @@ class Lab25 {
 		m2.start();
 	}
 }
+public class MultiThread extends Thread {
+	public void run() {		
+		int n = 5;
+		for(int i=0; i<n; i++) {
+System.out.println("Counting:"+i+" by Thread: "+Thread.currentThread().getId());
+			try {
+				Thread.sleep(1000);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
+
+
