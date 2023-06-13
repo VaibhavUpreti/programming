75e2b1b180759af7db404c89013f214253177d33
VaibhavUpreti <vaibhav.upreti16@gmail.com>
2023-06-13 20:11:31 +0530

    feat 26
    Signed-off-by: VaibhavUpreti <vaibhav.upreti16@gmail.com>

---
+++ java/Lab26b.java
@@ -0,0 +1,26 @@
+class Lab26b {
+	public static void main(String args[]) {
+System.out.println("Vaibhav Upreti 029");
+		MultiThread m1 = new MultiThread(5);
+		MultiThread m2 = new MultiThread(6);
+		m1.start();
+		m2.start();
+	}
+	public static synchronized void table(int n) {
+		for(int i=1; i<=10; i++) {
+System.out.println("Thread: "+Thread.currentThread().getId()+"\t"+n+" X "+i+" = "+n*i);
+		}
+	}
+}
+
+class MultiThread extends Thread {
+	int n;
+	MultiThread(int n) {
+		this.n = n;
+	}
+	public void run() {
+		Lab26b.table(n);
+	}
+}
+
+
